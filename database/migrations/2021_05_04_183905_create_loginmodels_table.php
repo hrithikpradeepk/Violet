d4c975fd7962953b3169a5a955aad2440ede5eb3
hrithikpradeepk <mca518@rajagiri.edu@gmail.com>
2021-05-27 23:04:32 +0530

    Welcome

--- database/migrations/2021_05_04_183905_create_loginmodels_table.php
+++ database/migrations/2021_05_04_183905_create_loginmodels_table.php
@@ -14,6 +14,7 @@ class CreateLoginmodelsTable extends Migration
     public function up()
     {
         Schema::create('loginmodels', function (Blueprint $table) {
+            $table->id("id");
             $table->string('Username')->index();
             $table->string('Password');
             $table->string("Usertype");
